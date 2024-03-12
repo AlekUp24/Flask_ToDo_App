d075d6296b68557912a582cedc89d9359b56a3e0
AlekUp24 <alekp24@gmail.com>
2024-03-12 10:32:29 +0100

    functionalities done, only css left

--- main.py
+++ main.py
@@ -22,12 +22,29 @@ def index():
     todo_list = ToDo.query.all()
     return render_template('base.html', todo_list = todo_list)
 
+
+# add new item
 @app.route('/add', methods=['POST'])
 def add():
-    # add new item
     title = request.form.get('title')
     new_todo = ToDo(title=title, complete=False)
     db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+# update item
+@app.route('/update/<int:todo_id>')
+def update(todo_id):
+    todo = ToDo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for('index'))
+
+# delete item
+@app.route('/delete/<int:todo_id>')
+def delete(todo_id):
+    todo = ToDo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
     db.session.commit()
     return redirect(url_for('index'))
 
