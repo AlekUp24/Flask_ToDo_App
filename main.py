c9be9e8c5dcbd4f36bdf01d7ae9bdba3e38076b7
AlekUp24 <alekp24@gmail.com>
2024-03-12 10:24:25 +0100

    update and create done

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 # from https://www.youtube.com/watch?v=yKHJsLUENl0&list=PLHocQMwDsV-jp3z7T4ousLoTpaM_3uGBs&index=23&t=96s&ab_channel=PatrickLoeber
-# 17:52 DEMO
+# 31:01 Delete
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
@@ -21,6 +21,15 @@ def index():
     # show all todos
     todo_list = ToDo.query.all()
     return render_template('base.html', todo_list = todo_list)
+
+@app.route('/add', methods=['POST'])
+def add():
+    # add new item
+    title = request.form.get('title')
+    new_todo = ToDo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     db.create_all()
